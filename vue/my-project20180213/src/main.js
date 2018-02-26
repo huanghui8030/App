beef8a9ee053949fa1eb10de8426b78ad9de0126
黄卉 <326028030@qq.com>
2018-02-26 15:58:05 +0800

    vue购物车实例

--- vue/my-project20180213/src/main.js
+++ vue/my-project20180213/src/main.js
@@ -1,8 +1,7 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import Tab from './Tab'
+import Cart from './Cart'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -21,4 +20,12 @@ new Vue({
   router,
   components: { Tab },
   template: '<Tab/>'
+})
+
+//购物车实例
+new Vue({
+  el: '#app3',
+  router,
+  components: { Cart },
+  template: '<Cart/>'
 })
